b9003ef1d5e88fcf92e4a1856e16f4322a889798
TIMM SNOW <two.tone.snow@gmail.com>
2021-02-17 13:32:39 -0500

    creates the hash

--- store_item.rb
+++ store_item.rb
@@ -1,1 +1,5 @@
 muffin = { :size => "large", :flavor => "banana nut", :price => "$5" }
+cookie = { :size => "x-large", :flavor => "red velvet", :price => "$3.50" }
+croissant = { size: "small", flavor: "almond", price: "$7" }
+
+p "#{croissant[:size]} #{croissant[:flavor]} croissants cost #{croissant[:price]}. Welcome to New York."
